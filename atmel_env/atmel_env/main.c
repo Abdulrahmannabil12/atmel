cc1003ca923da4d6ca76897c607b6eb4a5a3e113
abdulrahmannabil12 <abdulrahmannabil12@gmail.com>
2020-06-06 19:04:52 +0200

    add atmel project empty

---
+++ atmel_env/atmel_env/main.c
@@ -0,0 +1,18 @@
+/*
+ * atmel_env.c
+ *
+ * Created: 06/06/2020 06:58:37 م
+ * Author : ElpmDa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
