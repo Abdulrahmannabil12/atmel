4e0eeb31634d8c171873a9878f6de2035a2c2f12
abdulrahmannabil12 <abdulrahmannabil12@gmail.com>
2020-06-06 22:23:33 +0200

    led project with proteus file

--- atmel_env/atmel_env/main.c
+++ atmel_env/atmel_env/main.c
@@ -1,13 +1,17 @@
 
 
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRC = 1 << PC0; // make PC0 pin as output
     while (1) 
     {
+		PORTC |=  1 << PC0;
+		_delay_ms(1000);
+		PORTC &= ~(1<< PC0);
+		_delay_ms(1000);
     }
 }
 
