2140ffefb00d7e82d709f6d7940f78600809b089
abdulrahmannabil12 <abdulrahmannabil12@gmail.com>
2020-06-11 03:35:42 +0200

    led with butt updated

--- atmel_env/atmel_env/main.c
+++ atmel_env/atmel_env/main.c
@@ -3,15 +3,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define SET_BET(REG,BIT) (REG |=(1<<BIT))
+#define BIT_IS_SET(REG,BIT) (REG & ((1<<BIT)!=0))
+#define CLEAR_BIT(REG,BIT) (REG &= (~(1<<BIT)))
+
 int main(void)
 {
-    DDRC = 1 << PC0; // make PC0 pin as output
+    DDRC = 1 << PC0; 
     while (1) 
     {
-		PORTC |=  1 << PC0;
-		_delay_ms(1000);
-		PORTC &= ~(1<< PC0);
-		_delay_ms(1000);
+		if (BIT_IS_SET(PORTC,PC1))
+		{
+			SET_BET(PORTC,PC0);
+		}
+		else
+		{
+			CLEAR_BIT(PORTC,PC0);
+		}
+
     }
+	return(0);
 }
 
