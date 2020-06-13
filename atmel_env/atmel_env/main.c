7d6c97697c9dd1a869776e9d5dbb3bc902a036a0
abdulrahmannabil12 <abdulrahmannabil12@gmail.com>
2020-06-14 01:10:48 +0200

    2 leds with 2 but

--- atmel_env/atmel_env/main.c
+++ atmel_env/atmel_env/main.c
@@ -3,24 +3,33 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SET_BET(REG,BIT) (REG |=(1<<BIT))
-#define BIT_IS_SET(REG,BIT) (REG & ((1<<BIT)!=0))
+#define SET_BIT(REG,BIT) (REG |=(1<<BIT))
+#define BIT_IS_SET(REG,BIT) (REG & ((1<<BIT)))
 #define CLEAR_BIT(REG,BIT) (REG &= (~(1<<BIT)))
 
 int main(void)
 {
     DDRC = 1 << PC0; 
+	DDRC = 1 << PC7;
     while (1) 
     {
-		if (BIT_IS_SET(PORTC,PC1))
+		if (BIT_IS_SET(PINB,PB0))
 		{
-			SET_BET(PORTC,PC0);
+			SET_BIT(PORTC,PC0);
 		}
 		else
 		{
 			CLEAR_BIT(PORTC,PC0);
 		}
-
+        if (BIT_IS_SET(PINB,PB7))
+          {
+             SET_BIT(PORTC,PC7);   
+		   } 
+        
+        else
+        {
+			CLEAR_BIT(PORTC,PC7);
+        }
     }
 	return(0);
 }
