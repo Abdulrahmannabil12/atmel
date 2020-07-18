c701280584a1083a8e9836eaa5c7b66d38ba1cdd
mustafabahaa <mustafa.bahaa@valeo.com>
2020-07-18 05:11:47 +0200

    Fix interrupts compilation errors

--- atmel_env/atmel_env/MCAL/interupt/interrupt.c
+++ atmel_env/atmel_env/MCAL/interupt/interrupt.c
@@ -1,55 +1,69 @@
-/*
- * interrupt.c
- *
- * Created: 7/17/2020 3:22:02 AM
- *  Author: Abdulrahman Nabil
- */
+/**************************************************************************
+** This  software  is  in  the  public  domain , furnished "as is", without
+** technical support,  and with no  warranty, express or implied, as to its
+** usefulness for any purpose.
+
+** interrupt.c
+***************************************************************************
+**                              Includes                                 **
+**************************************************************************/
 #include "interrupt.h"
 
-interrupt_error MCAL_interrupt_initialize(
-	interrupt_t INT_X,
-	interrupt_t the_mode)
-{
+/*************************************************************************/
+/*                     Functions Implementation                          */
+/*************************************************************************/
 
-	/* set_bit(SREG,SREG_interrupt_bit);       */ /*enable interrupt*/
+interrupt_error mcal_interrupt_initialize(uint8_t interruptNo,
+										  interrupt_mode_t mode)
+{
 	interrupt_error error = INTERRUPT_STATE_SUCCESS;
 
-	switch (INT_X)
+	/*enable global interrupts*/
+	setGlobalInterrupt;
+
+	switch (interruptNo)
 	{
 
-	case (INT_X->bit == INT0):
-
-		switch (the_mode)
+	case INT0:
+	{
+		switch (mode)
 		{
-		case (the_mode->mode == falling_edge):
+		case RISING_EDGE:
+			set_bit(MCUCR, ISC00);
+			set_bit(MCUCR, ISC01);
+			break;
+
+		case FALLING_EDGE:
 			set_bit(MCUCR, ISC01);
 			clr_bit(MCUCR, ISC00);
 			break;
-		case (the_mode->mode == rising_edge):
-			set_bit(MCUCR, ISC00);
-			set_bit(MCUCR, ISC01);
-			break;
-		case (the_mode->mode == changing_edge):
+
+		case CHANGING_EDGE:
 			set_bit(MCUCR, ISC00);
 			clr_bit(MCUCR, ISC01);
 			break;
+
 		default:
 			error = INTERRUPT_STATE_INVALID_OPERATION_MODE;
 		}
 		break;
+	}
 
-	case (INT_X->bit == INT1):
-		switch (the_mode)
+	case INT1:
+	{
+		switch (mode)
 		{
-		case (the_mode->mode == falling_edge):
-			set_bit(MCUCR, ISC11);
-			clr_bit(MCUCR, ISC10);
-			break;
-		case (the_mode->mode == rising_edge):
+		case RISING_EDGE:
 			set_bit(MCUCR, ISC11);
 			set_bit(MCUCR, ISC10);
 			break;
-		case (the_mode->mode == changing_edge):
+
+		case FALLING_EDGE:
+			set_bit(MCUCR, ISC11);
+			clr_bit(MCUCR, ISC10);
+			break;
+
+		case CHANGING_EDGE:
 			set_bit(MCUCR, ISC10);
 			clr_bit(MCUCR, ISC11);
 			break;
@@ -58,45 +72,82 @@ interrupt_error MCAL_interrupt_initialize(
 			error = INTERRUPT_STATE_INVALID_OPERATION_MODE;
 		}
 		break;
-	case (INT_X->bit == INT2):
-		switch (the_mode)
+	}
+
+	case INT2:
+	{
+		switch (mode)
 		{
-		case (the_mode->mode == falling_edge):
-			clr_bit(MCUCSR, ISC2);
-			break;
-		case (the_mode->mode == rising_edge):
+		case RISING_EDGE:
 			set_bit(MCUCR, ISC2);
 			break;
+
+		case FALLING_EDGE:
+			clr_bit(MCUCSR, ISC2);
+			break;
+
 		default:
 			error = INTERRUPT_STATE_INVALID_OPERATION_MODE;
 		}
 		break;
-	default:
-		error = INTERRUPT_STATE_INVAILD_external_interrupt;
 	}
+
+	default:
+	{
+		error = INTERRUPT_STATE_INVAILD_INTERRUPT_NUMBER;
+	}
+	}
+	return error;
 }
 
-interrupt_error MCAL_interrupt_read(uint8_t *result, interrupt_flag *INTF_X)
+interrupt_error mcal_interrupt_read_flag(uint8_t flag, uint8_t *result)
 {
 	interrupt_error error = INTERRUPT_STATE_SUCCESS;
 	uint8_t value;
 
-	if (INTF_X->GIFR_bit == INTF0)
+	switch (flag)
 	{
-		*result = value;
-	}
-	else if (INTF_X->GIFR_bit == INTF1)
-	{
-		*result = !value;
-	}
-	/*	 else if (INTF_X->GIFR_bit == INTF1)
-		 { 
-			 
-		 }  */
-	else
-	{
-		error = INTERRUPT_STATE_INVAILD_external_interrupt;
+	case INTF0:
+		*result = bit_is_set(GIFR, INTF0);
+		break;
+
+	case INTF1:
+		*result = bit_is_set(GIFR, INTF1);
+		break;
+
+	case INTF2:
+		*result = bit_is_set(GIFR, INTF2);
+		break;
+
+	default:
+		error = INTERRUPT_STATE_INVAILD_INTERRUPT_NUMBER;
+		break;
 	}
 	return error;
 }
+
+interrupt_error mcal_interrupt_clear_flag(uint8_t flag)
+{
+	interrupt_error error = INTERRUPT_STATE_SUCCESS;
+	uint8_t value;
+
+	switch (flag)
+	{
+	case INTF0:
+		clr_bit(GIFR, INTF0);
+		break;
+
+	case INTF1:
+		clr_bit(GIFR, INTF1);
+		break;
+
+	case INTF2:
+		clr_bit(GIFR, INTF2);
+		break;
+
+	default:
+		error = INTERRUPT_STATE_INVAILD_INTERRUPT_NUMBER;
+		break;
+	}
+	return error;
 }
--- atmel_env/atmel_env/MCAL/interupt/interrupt.h
+++ atmel_env/atmel_env/MCAL/interupt/interrupt.h
@@ -1,14 +1,26 @@
-/*
- * interrupt.h
- *
- * Created: 7/17/2020 3:21:49 AM
- *  Author: Abdulrahman Nabil
- */
+/**************************************************************************
+** This  software  is  in  the  public  domain , furnished "as is", without
+** technical support,  and with no  warranty, express or implied, as to its
+** usefulness for any purpose.
 
-#include "standardtypes.h"
-
+** interrupt.h
+**************************************************************************/
 #ifndef INTERRUPT_H_
 #define INTERRUPT_H_
+
+/*************************************************************************/
+/*                              Includes                                 */
+/*************************************************************************/
+#include "standardtypes.h"
+
+/*************************************************************************/
+/*                              Macros                                   */
+/*************************************************************************/
+
+/*enable and disable interrupt by setting and clearing I bit in SREG */
+#define SREG_interrupt_bit 7
+#define setGlobalInterrupt set_bit(SREG, SREG_interrupt_bit)
+#define clearGlobalInterrupt clr_bit(SREG, SREG_interrupt_bit)
 
 /* define interrupt registers */
 #define SREG 0X5F
@@ -17,80 +29,73 @@
 #define MCUCR 0x55
 #define MCUCSR 0x54
 
-/*enable and disable interrupt by setting and clearing I bit in SREG */
-
-#define SREG_interrupt_bit 7
-#define setGlobalInterrupt set_bit(SREG, SREG_interrupt_bit)
-#define clearGlobalInterrupt clr_bit(SREG, SREG_interrupt_bit)
-
 /*MCUCR (MCU Control Register)*/
 #define ISC00 0
 #define ISC01 1
 #define ISC10 2
 #define ISC11 3
-/*MCUCSR (MCU Control Register)*/
 #define ISC2 6
 
-typedef enum
-{
-	/* GICR choose External interrupt*/
-	INT0 = 6 INT1 = 7 INT2 = 5
-} external_interrupt;
+/*Interrupts Number*/
+#define INT0 6
+#define INT1 7
+#define INT2 5
+
+/*Interrupts Flags*/
+#define INTF0 6
+#define INTF1 7
+#define INTF2 5
+
+/*************************************************************************/
+/*                               Types                                   */
+/*************************************************************************/
 
 typedef enum
 {
-	/* GIFR READ General Interrupt Flag Register*/
-	INTF0 = 6 INTF1 = 7 INTF2 = 5
-
-} interrupt_flag;
-
-typedef enum
-{
-	rising_edge,
-	falling_edge,
-	changing_edge
-} operat_mode_t;
+	RISING_EDGE,
+	FALLING_EDGE,
+	CHANGING_EDGE
+} interrupt_mode_t;
 
 typedef enum
 {
 	INTERRUPT_STATE_SUCCESS,
-
 	INTERRUPT_STATE_INVALID_OPERATION_MODE,
-
-	INTERRUPT_STATE_INVAILD_external_interrupt,
-
+	INTERRUPT_STATE_INVAILD_INTERRUPT_NUMBER
 } interrupt_error;
 
-typedef struct
-{
-
-	interrupt_flag GIFR_bit;
-	external_interrupt bit;
-	operat_mode_t mode;
-} interrupt_t;
 /*************************************************************************/
 /*                           Public Functions                            */
 /**************************************************************************
-	** MCAL_interrupt_initialize( ,)
-	**
-	** parameters: interrupt_t INT_X
-	** parameters: interrupt_t the_mode
-	** return    : interrupt_error
-	***************************************************************************
-	** this function is used to initialize all the necessary sequence for interrupt
-	**************************************************************************/
-interrupt_error MCAL_interrupt_initialize(interrupt_t INT_X,
-										  interrupt_t the_mode);
+** mcal_interrupt_initialize()
+**
+** parameters: uint8_t interruptNo
+** parameters: interrupt_mode_t mode
+** return    : interrupt_error
+***************************************************************************
+** this function is used to initialize all the necessary sequence for interrupt
+**************************************************************************/
+interrupt_error mcal_interrupt_initialize(uint8_t interruptNo,
+										  interrupt_mode_t mode);
 /**************************************************************************
-	** MCAL_interrupt_read(,)
-	**
-	** parameters: uint8_t  *result 
-	** parameters: interrupt_flag *INTF_X 
-	** return    : interrupt_error
-	***************************************************************************
-	** this function is used read a value from GIFR register
-	**************************************************************************/
-interrupt_error MCAL_interrupt_read(uint8_t *result,
-									interrupt_flag *INTF_X);
+** mcal_interrupt_read_flag()
+**
+** parameters: uint8_t flag
+** parameters: uint8_t *result
+** return    : interrupt_error
+***************************************************************************
+** this function is used read a interrupts flags from GIFR register
+**************************************************************************/
+interrupt_error mcal_interrupt_read_flag(uint8_t flag , uint8_t *result);
+
+/**************************************************************************
+** mcal_interrupt_clear_flag()
+**
+** parameters: uint8_t flag
+** return    : interrupt_error
+***************************************************************************
+** this function is used to clear flag from GIFR register
+**************************************************************************/
+interrupt_error mcal_interrupt_clear_flag(uint8_t flag);
 
 #endif /* INTERRUPT_H_ */
